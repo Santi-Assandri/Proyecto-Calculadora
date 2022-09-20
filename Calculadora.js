fade2be51707e509db318d4189cd982cf35adca0
Santi-Assandri <srodassandri@gmail.com>
2022-09-20 11:19:55 -0300

    Titulo del proyecto

--- Calculadora.js
+++ Calculadora.js
@@ -3,20 +3,20 @@ const multiplicar = require("./funciones/multiplicacion.js");
 const suma = require("./funciones/suma.js");
 const restar = require("./funciones/resta.js");
 
-num1 = 6
-num2 = 9
+let num1 = 6
+let num2 = 9
 const resultado = dividir(num1,num2)
 console.log(" Realizamos la division de dos numeros: ")
 console.log(resultado)
 
-parametro1 = 7 
-parametro2 = 25
+let parametro1 = 7 
+let parametro2 = 25
 const resultado2 = restar(parametro1,parametro2)
  console.log("Realizamos la resta de dos numero: ")
  console.log(parametro2 + " - " + parametro1 + " = " + resultado2)
 
-numero1 = 6
-numero2 = 6
+let numero1 = 6
+let numero2 = 6
  const resultado3 = multiplicar(numero1,numero2)
 console.log(" Realizamos la multiplicacion de dos numeros: ")
 console.log(numero1 + " * " + numero2 + " = " + resultado3)
